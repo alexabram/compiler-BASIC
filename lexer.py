1c7db668aea91c88a9a89e818e2459cc12ab20c6
Alex Abram <alexabram@xelaMBP16.lan>
2024-01-14 09:41:37 -0800

    add: token types

--- lexer.py
+++ lexer.py
@@ -1,32 +1,57 @@
 class Lexer:
-  self.source = source + '\n' # Source code to lex as a string. Append a newline to simplify lexing/parsing the last token/statement.
-  self.curChar = ''   # Current character in the string.
-  self.curPos = -1    # Current position in the string.
-  self.nextChar()
+    def __init__(self, source):
+        self.source = (
+            source + "\n"
+        )  # Source code to lex as a string. Append a newline to simplify lexing/parsing the last token/statement.
+        self.curChar = ""  # Current character in the string.
+        self.curPos = -1  # Current position in the string.
+        self.nextChar()
 
-  def __init__(self, source):
-    pass
+    # Process the next character.
+    def nextChar(self):
+        self.curPos += 1
+        self.curChar = (
+            "\0" if self.curPos >= len(self.source) else self.source[self.curPos]
+        )
 
-  # Process the next character.
-  def nextChar(self):
-    pass
+    # Return the lookahead character.
+    def peek(self):
+        return (
+            "\0"
+            if self.curPos + 1 >= len(self.source)
+            else self.source[self.curPos + 1]
+        )
 
-  # Return the lookahead character.
-  def peek(self):
-    pass
+    # Invalid token found, print error message and exit.
+    def abort(self, message):
+        pass
 
-  # Invalid token found, print error message and exit.
-  def abort(self, message):
-    pass
-  
-  # Skip whitespace except newlines, which we will use to indicate the end of a statement.
-  def skipWhitespace(self):
-    pass
-  
-  # Skip comments in the code.
-  def skipComment(self):
-    pass
+    # Skip whitespace except newlines, which we will use to indicate the end of a statement.
+    def skipWhitespace(self):
+        pass
 
-  # Return the next token.
-  def getToken(self):
-    pass
+    # Skip comments in the code.
+    def skipComment(self):
+        pass
+
+    # Return the next token.
+    def getToken(self):
+        # Check the first character of this token to see if we can decide what it is.
+        # If it is a multiple character operator (e.g., !=), number, identifier, or keyword then we will process the rest.
+        if self.curChar == "+":
+            pass  # Plus token.
+        elif self.curChar == "-":
+            pass  # Minus token.
+        elif self.curChar == "*":
+            pass  # Asterisk token.
+        elif self.curChar == "/":
+            pass  # Slash token.
+        elif self.curChar == "\n":
+            pass  # Newline token.
+        elif self.curChar == "\0":
+            pass  # EOF token.
+        else:
+            # Unknown token!
+            pass
+
+        self.nextChar()
